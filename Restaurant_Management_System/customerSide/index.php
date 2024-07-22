3bb4b3cdfe9e82824a39073cbbade74d4dd5200b
Nithh <114797570+Nithhzzy@users.noreply.github.com>
2024-07-22 16:58:22 +0000

    Upload Files ✅

--- Restaurant_Management_System/customerSide/index.php
+++ Restaurant_Management_System/customerSide/index.php
@@ -1,1 +1,18 @@
+<?php 
+// Remember to change the username, password, and database name to actual values
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'restaurantDB');
 
+// Create Connection
+$link = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+// Check Connection
+if ($link->connect_error) {
+    // if no connection, kill the connection or terminate execution
+    die('Connection Failed: ' . $link->connect_error);
+} else {
+    echo "Connected successfully";
+}
+?>
