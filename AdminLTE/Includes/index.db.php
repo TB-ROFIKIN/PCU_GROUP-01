e2d4422603172aa7caf76bdd2115e02b5ef8c3b3
Nithh <114797570+Nithhzzy@users.noreply.github.com>
2024-08-19 11:55:08 +0000

    Second changes!

--- AdminLTE/Includes/index.db.php
+++ AdminLTE/Includes/index.db.php
@@ -1,9 +1,9 @@
 <?php
 class Database {
     private $host = 'localhost';
-    private $db_name = 'your_database_name';
-    private $username = 'your_db_username';
-    private $password = 'your_db_password';
+    private $db_name = 'AdminLTE';
+    private $username = 'localhost';
+    private $password = '';
     public $conn;
 
     public function getConnection() {
